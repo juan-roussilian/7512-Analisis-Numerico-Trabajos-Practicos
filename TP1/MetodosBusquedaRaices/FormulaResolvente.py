06064bd948d9f11e5bc363ff6d85f79ca1632de0
Juan Cruz <juancruzroussilian9@gmail.com>
2020-11-09 18:24:25 -0300

    Cambio nombre de var aux a discriminante

--- TP1/MetodosBusquedaRaices/FormulaResolvente.py
+++ TP1/MetodosBusquedaRaices/FormulaResolvente.py
@@ -11,11 +11,11 @@ class FormulaResolvente:
         return self.__aplicarBhaskara()
 
     def __aplicarBhaskara(self):
-        aux = math.pow(self.coeficiente2doGrado, 2) - (4 * self.coeficiente1erGrado * self.coeficiente3erGrado)
+        discriminante = math.pow(self.coeficiente2doGrado, 2) - (4 * self.coeficiente1erGrado * self.coeficiente3erGrado)
 
-        if aux >= 0:
-            primerRaiz = (-self.coeficiente2doGrado) + (math.sqrt(aux)/2 * self.coeficiente1erGrado)
-            segundaRaiz = (-self.coeficiente2doGrado) - (math.sqrt(aux)/2 * self.coeficiente1erGrado)
+        if discriminante >= 0:
+            primerRaiz = (-self.coeficiente2doGrado) + (math.sqrt(discriminante)/2 * self.coeficiente1erGrado)
+            segundaRaiz = (-self.coeficiente2doGrado) - (math.sqrt(discriminante)/2 * self.coeficiente1erGrado)
 
             if primerRaiz == segundaRaiz:
                 return primerRaiz
