4cd253cf1876342b75f6d1fbf69e4ab402c5f21e
Juan Cruz <juancruzroussilian9@gmail.com>
2020-11-18 12:13:36 -0300

    Hago funcion base de p. fijo

--- TP1/MetodosBusquedaRaices/PuntoFijo.py
+++ TP1/MetodosBusquedaRaices/PuntoFijo.py
@@ -7,28 +7,27 @@ def x(x):
     return x
 
 def punto_fijo_rec(g, semilla, tolerancia, iteraciones):
-    return
+    if abs(g(semilla)) <= tolerancia or iteraciones == 0:
+        return semilla
+    return punto_fijo_rec(g, g(semilla), tolerancia, iteraciones - 1)
 
 def obtener_g(f):
-
     x = symbols('x')
-    expresion = x - sympify(str(f()))
+    expresion = x - sympify(f)
     return lambdify(x, expresion)
 
 
-def punto_fijo(f,intervalo, tolerancia, iteraciones):
+def punto_fijo(f, intervalo, tolerancia, iteraciones):
 
     g = obtener_g(f)
-    semilla = biseccion(g, intervalo,tolerancia, iteraciones)
-
+    semilla = biseccion(g, intervalo, tolerancia,2)
+    print("La raiz hallada como semilla es: " + str(semilla))
     return punto_fijo_rec(g, semilla, tolerancia, iteraciones)
 
 def existe_unico_p_fijo(g, intervalo):
     g_prima = g(symbols('x')).diff(x)
-
-def lineal(x):
-    return  x/2
+    return true
 
 if __name__ == "__main__":
 
-    punto_fijo(lineal, (0,1), 1e-2, 2)
+    print("la raiz hallaada con p fijo es: " +  str(punto_fijo("x ** 2 - 1", (0, 2), 1e-2, 200)))
