3db93b05e4be7d3fd0bfed412d3a26f6af17b342
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2020-11-19 19:02:45 -0300

    Arreglo representación historias

--- TP1/MetodosBusquedaRaices/RepresentacionHistorias.py
+++ TP1/MetodosBusquedaRaices/RepresentacionHistorias.py
@@ -6,33 +6,36 @@ MAXTABLA = 12
 
 def recortar_historia(hist):
     aux = hist[:6] + hist[-6:]
-    print(len(aux))
     return aux
 
 
 def tabular_historia(historia, nombre_tabla):
-    print(len(historia))
+    it_totales = len(historia)
+    print(it_totales)
+    corte = 7
     if len(historia) > MAXTABLA:
+        corte = len(historia) + 1 - 6
         historia = recortar_historia(historia)
 
     print("Tabulando método " + nombre_tabla + "\n")
 
     header = ['Iteracion', 'Aproximacion']
-    iteraciones = range(1, len(historia) + 1)
+    iteraciones = [*range(1, 7)] + [*range(corte, it_totales)]
     table = zip(iteraciones, historia)
 
-    print(tabulate(table, headers=header))
+    print(tabulate(table, headers=header, floatfmt=".16f"))
 
 
 def graficar_historias(diccionario_metodos_raices):
-    #    cantidad_iteraciones = len(lista_iteraciones) + 1
     plt.title("Comparación entre métodos de búsqueda de raíces")
-    plt.xlabel('Paso [n]')
+    plt.ylabel('Estimacion raiz [n]')
+    plt.xlabel('Iteracion [n]')
     plt.grid(True)
     plt.legend(loc='best')
     plt.show()
     plt.figure()
 
     for metodo in diccionario_metodos_raices:
+        print(metodo)
         historia = recortar_historia(diccionario_metodos_raices[metodo])
         plt.plot(range(MAXTABLA), historia, '-', lw=2, label=metodo)
