6ef14e04b37a50b5e285232efa9849ec5774d1a3
Juan Cruz <juancruzroussilian9@gmail.com>
2020-11-20 20:14:20 -0300

    agrego parametro opcional al graficar para decidir tipo de escala en eje Y

--- TP1/MetodosBusquedaRaices/RepresentacionHistorias.py
+++ TP1/MetodosBusquedaRaices/RepresentacionHistorias.py
@@ -30,7 +30,7 @@ def tabular_historia_precision5(historia, nombre_tabla, it_totales):
     print("La estimación final por el método " + nombre_tabla + " de la raíz fue" + "%.5f" % aux[it_totales - 1] + "\n\n\n")
 
 
-def graficar(diccionario_historia):
+def graficar(diccionario_historia, escalaY="linear"):
     plt.title("Comparación entre métodos de búsqueda de raíces")
     plt.ylabel('Estimacion raiz [n]')
     plt.xlabel('Iteracion [n]')
@@ -38,6 +38,7 @@ def graficar(diccionario_historia):
     plt.legend(loc='best')
     plt.show()
     plt.figure()
+    plt.yscale(escalaY)
     for metodo in diccionario_historia:
         historia = diccionario_historia[metodo]
         print(historia)
