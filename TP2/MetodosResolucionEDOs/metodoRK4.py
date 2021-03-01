da526beff57bc0e8f50132850cc847def8a61509
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2021-03-01 16:08:18 -0300

    Creo el main para ingresar los parámetros y formateo el archivo

--- TP2/MetodosResolucionEDOs/metodoRK4.py
+++ TP2/MetodosResolucionEDOs/metodoRK4.py
@@ -1,49 +1,45 @@
 # RK-4 method python program
 
 # function to be solved
-def f(x, y, a, b):
-    return a * x + b * x * y
+
+# primera ecuación diferencial del sistema depredador-presa
+def f_dif(a, b):
+    return lambda x, y: a * x + b * x * y
 
 
-def g(x, y, c, d):
-    return c * x * y - d * y
+# segunda ecuación diferencial del sistema depredador-presa
+def g_dif(c, d):
+    return lambda x, y: c * x * y - d * y
 
-
-# or
-# f = lambda x: x+y
 
 # RK-4 method
-def rk4(x0, y0, xn, ti, tf):
+def rk4(x0, y0, h, ti, tf, f, g):
     # Calculating step size
-    h = 0.1
 
     print('\n--------SOLUTION--------')
     print('-------------------------')
-    print('x0\ty0\tyn')
+    print('x0\ty0\txn\tyn')
     print('-------------------------')
 
-    # falta mandar a,b,c,d en cada llamada a las funciones g y f.
-    # Creo que lo mejor sería tener funciones que reciban a,b,c,d de parámetro y devuelvan expresiones lambda ya
-    # formadas con esos params.
-
     while ti < tf:
-        k1 = (f(x0, y0))
-        k2 = (f((x0 + h / 2), (y0 + k1 / 2)))
-        k3 = (f((x0 + h / 2), (y0 + k2 / 2)))
-        k4 = (f((x0 + h), (y0 + k3)))
+        k1 = f(x0, y0)
+        k2 = f((x0 + h / 2), (y0 + k1 / 2))
+        k3 = f((x0 + h / 2), (y0 + k2 / 2))
+        k4 = f((x0 + h), (y0 + k3))
         k = (k1 + 2 * k2 + 2 * k3 + k4) * (h / 6)
 
-        m1 = (g(x0, y0))
-        m2 = (g((x0 + h / 2), (y0 + m1 / 2)))
-        m3 = (g((x0 + h / 2), (y0 + m2 / 2)))
-        m4 = (g((x0 + h), (y0 + m3)))
+        m1 = g(x0, y0)
+        m2 = g((x0 + h / 2), (y0 + m1 / 2))
+        m3 = g((x0 + h / 2), (y0 + m2 / 2))
+        m4 = g((x0 + h), (y0 + m3))
         m = (m1 + 2 * m2 + 2 * m3 + m4) * (h / 6)
 
         yn = y0 + k
         xn = x0 + m
 
-        # print('%.4f\t%.4f\t%.4f' % (x0, y0, yn))
-        # print('-------------------------')
+        print('%.4f\t%.4f\t%.4f\t%.4f' % (x0, y0, xn, yn))
+        print('-------------------------')
+
         y0 = yn
         x0 = xn
         ti = ti + h
@@ -52,15 +48,34 @@ def rk4(x0, y0, xn, ti, tf):
 
 
 # Inputs
-print('Enter initial conditions:')
-x0 = float(input('x0 = '))
-y0 = float(input('y0 = '))
+if __name__ == "__main__":
+    print('Ingrese las condiciones iniciales:')
+    x_inicial = float(input('x0 = '))
+    y_inicial = float(input('y0 = '))
 
-print('Enter calculation point: ')
-xn = float(input('xn = '))
+    print('Ingrese la razón de crecimiento de las presas:')
+    crecimiento_presa = float(input('a = '))
 
-print('Enter number of steps:')
-step = int(input('Number of steps = '))
+    print('Ingrese los parámetros de la interacción para la muerte de presas:')
+    muerte_presas = float(input('b = '))
 
-# RK4 method call
-rk4(x0, y0, xn, step)
+    print('Ingrese la razón de muerte de los depredadores:')
+    muerte_depredadores = float(input('c = '))
+
+    print('Ingrese los parámetros de la interacción para el crecimiento del depredador:')
+    crecimiento_depredadores = float(input('d = '))
+
+    print('Ingrese el incremento de cada paso:')
+    h_incremento = float(input('h = '))
+
+    print('Ingrese el tiempo inicial')
+    t_inicial = float(input('ti= '))
+
+    print('Ingrese el tiempo final')
+    t_final = float(input('tf= '))
+
+    funcion_f = f_dif(crecimiento_presa, muerte_presas)
+    funcion_g = g_dif(muerte_depredadores, crecimiento_depredadores)
+
+    # RK4 method call
+    rk4(x_inicial, y_inicial, h_incremento, t_inicial, t_final, funcion_f, funcion_g)
