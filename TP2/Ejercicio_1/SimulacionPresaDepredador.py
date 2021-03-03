e71011c85abf3d56b65c975a890655a2a2485b61
Juan Cruz <juancruzroussilian9@gmail.com>
2021-03-03 16:51:43 -0300

    Agrego logica de validacion de inputs de usuario de la TUI

--- TP2/Ejercicio_1/SimulacionPresaDepredador.py
+++ TP2/Ejercicio_1/SimulacionPresaDepredador.py
@@ -12,39 +12,73 @@ def f_dif(a, b):
 def g_dif(c, d):
     return lambda x, y: c * x * y - d * y
 
+def pedir_input_numerico(nombreParametro):
+    input_es_valido = False
+    input_usuario = None
+    while(not input_es_valido):
+        try:
+            input_usuario = float(input(nombreParametro +' = '))
+            input_es_valido = True
+        except ValueError:
+            print('No se ingreso un valor numerico')
+    return input_usuario
+
+def pedir_respuesta_afirmativa_negativa():
+    input_es_valido = False
+    input_usuario = None
+    while (not input_es_valido):
+            input_usuario = input('S/N ')
+            if(input_usuario == 'S' or input_usuario == 'N'):
+                input_es_valido = True
+            else:
+                print('Comando no valido')
+    return input_usuario
+
+
 if __name__ == "__main__":
-    print('Ingrese las condiciones iniciales:')
-    x_inicial = float(input('x0 = '))
-    y_inicial = float(input('y0 = '))
+    print('Desea realizar la simulacion con los parametros del item A?')
 
-    print('Ingrese la razón de crecimiento de las presas:')
-    crecimiento_presa = float(input('a = '))
+    if(pedir_respuesta_afirmativa_negativa() == 'S'):
+        x_inicial=2
+        y_inicial=1
+        crecimiento_presa=1.2
+        muerte_presas=0.6
+        muerte_depredadores=0.3
+        crecimiento_depredadores=0.8
+        h_incremento=0.1
+        t_inicial=0
+        t_final=30
 
-    print('Ingrese los parámetros de la interacción para la muerte de presas:')
-    muerte_presas = float(input('b = '))
+    else:
+        print('Ingrese las condiciones iniciales:')
+        x_inicial = pedir_input_numerico('x0')
+        y_inicial = pedir_input_numerico('y0')
 
-    print('Ingrese la razón de muerte de los depredadores:')
-    muerte_depredadores = float(input('c = '))
+        print('Ingrese la razón de crecimiento de las presas:')
+        crecimiento_presa = float(input('a = '))
 
-    print('Ingrese los parámetros de la interacción para el crecimiento del depredador:')
-    crecimiento_depredadores = float(input('d = '))
+        print('Ingrese los parámetros de la interacción para la muerte de presas:')
+        muerte_presas = pedir_input_numerico('b')
 
+        print('Ingrese la razón de muerte de los depredadores:')
+        muerte_depredadores = pedir_input_numerico('c')
+
+        print('Ingrese los parámetros de la interacción para el crecimiento del depredador:')
+        crecimiento_depredadores = pedir_input_numerico('d')
+
+        print('Ingrese el incremento de cada paso:')
+        h_incremento = pedir_input_numerico('h')
+
+        print('Ingrese el tiempo inicial')
+        t_inicial = pedir_input_numerico('ti')
+
+        print('Ingrese el tiempo final')
+        t_final = pedir_input_numerico('tf')
+
+    # Creo las funciones en base a los parametros
     funcion_f = f_dif(crecimiento_presa, muerte_presas)
     funcion_g = g_dif(muerte_depredadores, crecimiento_depredadores)
-
-    print(funcion_f)
-    print(funcion_g)
-
-    print('Ingrese el incremento de cada paso:')
-    h_incremento = float(input('h = '))
-
-    print('Ingrese el tiempo inicial')
-    t_inicial = float(input('ti= '))
-
-    print('Ingrese el tiempo final')
-    t_final = float(input('tf= '))
-
-    # llamo al metodo
+    # Llamo al metodo
     historias_graficos = rk4(x_inicial, y_inicial, h_incremento, t_inicial, t_final, funcion_f, funcion_g)
 
     # reutilizamos el mismo graficador del TP1 \(★ω★)/
